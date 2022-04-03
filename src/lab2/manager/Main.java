94999760ed500daa9fc307f2141f738d94c979ad
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-04-03 21:54:14 +0300

    Lab 3

---
+++ src/lab2/manager/Main.java
@@ -0,0 +1,12 @@
+package lab2.manager;
+
+import static lab2.manager.Manager.sortingByPower;
+import static lab2.manager.Manager.sortingByYear;
+
+public class Main {
+
+    public static void main(String[] args) {
+            sortingByPower();
+            sortingByYear();
+    }
+}
